155a05977539f5bb035e1d822ed638f9ceaaaa34
Chitransh2727 <108145764+Chitransh2727@users.noreply.github.com>
2022-07-01 15:11:40 +0530

    Create sorted.js

---
+++ sorted.js
@@ -0,0 +1,8 @@
+# Relevel-dout-
+let array = ["cat", "banana" , "parrot"] 
+
+// let sortedarray = array.sort();
+let reversearray = array.reverse();
+console.log(reversearray);
+
+RESULT ---------------------->>>>> parrot , banana , cat 
